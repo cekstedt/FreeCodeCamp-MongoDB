eb0c4197215e1b98d99522fb4d57cc057760f230
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-11-14 16:17:33 -0800

    Add findOneByFood() function.

--- myApp.js
+++ myApp.js
@@ -35,7 +35,7 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, done);
 };
 
 const findPersonById = (personId, done) => {
